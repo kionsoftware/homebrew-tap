1df9e441f9518db03ae5abd46728f0cb5f715fc5
kionbot <noreply+kionbot@kion.io>
2024-06-03 14:15:05 +0000

    Automated Kion-CLI release v0.3.0

--- Formula/kion-cli.rb
+++ Formula/kion-cli.rb
@@ -4,11 +4,11 @@
 class KionCli < Formula
   desc "Kion on the command-line!"
   homepage "https://github.com/kionsoftware/kion-cli"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
-  url "https://github.com/kionsoftware/kion-cli/archive/v0.2.1.tar.gz"
-  sha256 "f733305acf71e3d4a261d6bf1eed8d0e4f11ef279ca556ab1a53d759a0e73f62"
+  url "https://github.com/kionsoftware/kion-cli/archive/v0.3.0.tar.gz"
+  sha256 "f408eca149444d828b8eff6fe7ac5af2fdf418244148d4cb54edd84324f4f508"
   head "https://github.com/kionsoftware/kion-cli"
 
   depends_on "go" => :build
